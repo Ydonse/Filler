a09881f3322cc9440efd52a0b4382eaef93daf1f
Ydonse <ydonse@student.42.fr>
2020-02-14 13:24:36 +0100

    test

--- src/visu.c
+++ src/visu.c
@@ -1,38 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   visu.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ydonse <ydonse@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/07/28 13:26:55 by ydonse            #+#    #+#             */
-/*   Updated: 2020/02/11 16:01:23 by ydonse           ###   ########.fr       */
+/*   Created: 2020/02/14 13:21:15 by ydonse            #+#    #+#             */
+/*   Updated: 2020/02/14 13:23:05 by ydonse           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "filler.h"
-#include <stdio.h>
-#include <stdlib.h>
 
-int	main(int argc, char **argv)
+void	init_visu()
 {
-	char 	*tab;
-	t_main 	s;
-	int		fd;
+	
+}
 
-	s.player = get_player_nb(&s);
-	tab = fill_tab();
-	fd = open();
-	//s.board = get_board_size(&s);
-	// while (1)
-	// {
-	// 	//get_piece
-	// 	//place_piece
-	// 	//return_piece_pos
-	// }
-	ft_putstr(tab);
-	write(1, tab, ft_strlen(tab));
+void	display_visu()
+{
 
-	// ft_putstr(tab);
-	return(0);
 }
